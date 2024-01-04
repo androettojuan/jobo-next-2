57d33b267f13ed9c1ee74177b86739d9106e5dbe
Juan Manuel Androetto <juan.androetto36@gmail.com>
2024-01-04 16:23:03 -0300

    profesional card

--- src/app/profession/page.jsx
+++ src/app/profession/page.jsx
@@ -1,8 +1,17 @@
+import ProfesionalCard from "@/components/ProfesionalCard";
 
 const ProfessionPage = () => {
     return (
-        <div>
-            <h1>Profession Page</h1>
+        <div className="flex flex-col items-start">
+            <h3 className="text-2xl font-bold pl-2 mt-5">Gasista</h3>
+            <ProfesionalCard
+                key={"1"}
+                name={"Juan Manuel Androetto"}
+                profession={"Gasista"}
+                lastname={"Androetto"}
+                photo={"/img/plomero-foto.jpg"}
+                rating={"5"}
+              />
         </div>
     );
 };
---
+++ src/components/ProfesionalCard.jsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import React from "react";
+import { AiFillHeart, AiFillStar, AiOutlineHeart } from "react-icons/ai";
+
+const ProfesionalCard = ({
+  name,
+  profession,
+  photo,
+  onClick,
+  favorite,
+  isFavorite,
+  rating
+}) => {
+  return (
+    <div className="flex flex-row shadow-md rounded-xl h-24 cursor-pointer w-full" onClick={onClick}>
+      <Image className="rounded-tl-2xl rounded-bl-2xl object-cover" width={110} height={90} src={photo} alt="foto profesional" />
+      <div className="flex flex-row justify-between w-full p-2 px-4">
+        <div className="flex flex-col items-start ">
+          <span className="font-bold text-xl mb-1 ">{name}</span>
+          <span className="font-normal text-xl">{profession}</span>
+        </div>
+        <div className="flex flex-col items-end justify-between">
+          <div className="text-[red] text-2xl" onClick={isFavorite}>
+            {favorite ? <AiFillHeart /> : <AiOutlineHeart />}
+          </div>
+          <div className="flex flex-row items-center">
+            <div className="text-[#FFCF26] text-2xl mr-1">
+              <AiFillStar />
+            </div>
+            <span className="font-normal text-xl">{rating}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfesionalCard;
